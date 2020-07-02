d6727100e7b1d201bc78ff25d7b9e66ff8a837aa
binom989 <binom9@yandex.ru>
2020-07-02 11:43:18 +0400

    от 02,07

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,31 @@
-/* for (let i = 1; i < 8; i++) {
-    console.log(i) 
-} */
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        /* dots: true,  точки под картинкой*/
+        infinite: true,
+        speed: 1200,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+ 
+        responsive: [
+            {
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+            }
+        ]
 
-function logging() {
-    console.log(222);
-}
 
-logging();
+
+        /* arrows: false, выключение стрелок */ 
+
+        /* autoplay: true,
+        autoplaySpeed: 2000  автоскроллинг*/
+
+        /* fade: true,   
+        cssEase: 'linear'  */ /* проявление картинки */
+      });
+  });
